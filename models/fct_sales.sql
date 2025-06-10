68e6f6154163c234c2f80b1bb87d45145acc276f
zahero3 <zahamad95@gmail.com>
2025-06-10 20:33:42 +0530

    Added net_revenue and its test

--- models/fct_sales.sql
+++ models/fct_sales.sql
@@ -13,4 +13,4 @@ SELECT
     (total_amount - round((o.quantity * p.price) * 0.1,2)) AS net_revenue
 FROM {{ ref('stg_orders') }} o
 JOIN {{ ref('stg_customers') }} c ON o.customer_id = c.customer_id
-JOIN {{ ref('stg_products') }} p ON o.product_id = p.product_id
+JOIN {{ ref('stg_products') }} p ON o.product_id = p.product_id 
